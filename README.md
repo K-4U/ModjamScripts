9c07eaa2bfd6abbb328cf1db8d7d7541c597a4f5
K-4U <koen@k-4u.nl>
2013-12-09 22:59:39 +0100

    Added a commit script as well! :)

--- README.md
+++ README.md
@@ -1,21 +1,28 @@
-ModjamScripts
-=============
-
+#ModjamScripts
 Some scripts to use in your stream with Modjam!
 
-Prerequisites:
-==============
+Sorry. There is no script (yet) for people who use bitbucket. They can simply disable the github lookup.
+##Prerequisites
+
+####For streamScript 
 Githubv3 API for python:
 [https://github.com/copitux/python-github3](https://github.com/copitux/python-github3)
 
-Install:
-========
+###For commitScript
+None!
+
+##Install
+###For streamScript
 Checkout the python-github3 and install that.
 
 Place the streamScript.py in the directory where you want your files to be generated.
 
-Config:
-=======
+###For commitScript
+Place it in a directory. Doesn't really matter.. (Not even in the end!)
+
+
+##Config
+###For streamScript
 Just edit the streamScript.py file. 
 Lets take this github URL:
 > https://github.com/K-4U/ModjamScripts
@@ -29,11 +36,32 @@ There isn't any more to it!
 
 Note: As of now there is no option of where to place the textfiles!
 
+###For commitScript
+You only need to edit the path where your working copy is located.
 
-Usage:
-======
+For windows users: Use forward slashes.
+So if your working copy is on 
+>E:\Repositories\Modjam\
+
+Your config will be this:
+
+	repoDir = "E:/Repositories/Modjam"
+There's also a print_debug. Set this to false if you don't want the script to print out messages about which files have changed.
+
+##Usage
+###For streamScript
 Just run the script with python:
 
 > python streamScript.py
 
 Please take note that the Github API only allows for 60 requests every hour (per IP)! Do not change the timer at the bottom!
+
+###For commitScript
+The same. Just run the script with python:
+
+> python commitScript.py
+
+It'll print out the time that is left before making a commit. It will do this in minutes!
+
+##Example
+![](https://dl.dropboxusercontent.com/u/343724/MC/Modjam/streamScriptPreview.png)
---
+++ commitScript.py
@@ -0,0 +1,105 @@
+# Author: K-4U
+# Written for Modjam 3
+# This script will commit your files to the git repository every 15 minutes
+
+repoDir = "/Path/To/Your/Working/Copy"
+print_debug = True
+
+# DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING!
+
+import time
+import os
+import string
+from subprocess import *
+
+def command(x):
+    if(print_debug):
+        print(x)
+    return str(Popen(x, stdout=PIPE).communicate()[0])
+
+def rm_empty(L): return [l for l in L if (l and l!="")]
+
+def getUntracked():
+    os.chdir(repoDir)
+    status = command("git status")
+    if "# Untracked files:" in status:
+        untf = status.split("# Untracked files:")[1][1:].split("\n")
+        return rm_empty([x[2:] for x in untf if string.strip(x) != "#" and x.startswith("#\t")])
+    else:
+        return []
+
+def getNew():
+    os.chdir(repoDir)
+    status = command("git status").split("\n")
+    return [x[14:] for x in status if x.startswith("#\tnew file:   ")]
+
+def getModified():
+    os.chdir(repoDir)
+    status = command("git status").split("\n")
+    return [x[14:] for x in status if x.startswith("#\tmodified:   ")]
+
+def getDeleted():
+    os.chdir(repoDir)
+    status = command("git status").split("\n")
+    return [x[14:] for x in status if x.startswith("#\tdeleted: ")]
+
+def add(files):
+    os.chdir(repoDir)
+    if len(files) > 0:
+        files2 = []
+        for f in files:
+            if f.find(' '):
+                f = '"' + f + '"'
+            files2.append(f)
+        command("git add %s" % ('*'.join(files2).replace("*"," ")))
+
+def remove(files):
+    os.chdir(repoDir)
+    if len(files) > 0:
+        files2 = []
+        for f in files:
+            if f.find(' '):
+                f = '"' + f + '"'
+            files2.append(f)
+
+        command("git rm %s" % ('*'.join(files2).replace("*"," ")))
+
+def commit(logMessage):
+    os.chdir(repoDir)
+    command("git commit -m\"%s\"" % logMessage)
+
+def push():
+    os.chdir(repoDir)
+    command("git push")
+
+
+timeCount = 1
+while 1:
+    timeCount -= 1
+    print("Committing to repository in %d minutes!"%timeCount)
+    if(timeCount == 0):
+        timeCount = 15
+        #This should run about every 15 minutes..
+        untracked = getUntracked()
+        new = getNew()
+        modified = getModified()
+        deleted = getDeleted()
+        if(print_debug):
+            print("Untracked:")
+            print( untracked )
+            print("New:")
+            print( new )
+            print("Modified:")
+            print( modified )
+            print("Deleted:")
+            print( deleted )
+
+
+        add(untracked)
+        add(modified)
+        remove(deleted)
+
+        if(len(deleted) > 0 or len(modified) > 0 or len(untracked) > 0):
+            commit("commit from %s. %d deletions, %d adds, %d modifications" % (time.strftime("%c"), len(deleted), len(untracked), len(modified)))
+            push()
+    time.sleep(60)
